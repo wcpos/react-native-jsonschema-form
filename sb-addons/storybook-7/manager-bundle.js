d3d2288958dfeea165d544ad926a153fef8cc53a
kilbot <kilbot@users.noreply.github.com>
2023-12-03 11:29:17 +0000

    deploy: 550eaf52b7075845434720913add21bfa5c2f9d9

---
+++ sb-addons/storybook-7/manager-bundle.js
@@ -0,0 +1,3 @@
+try{
+(()=>{var h=__STORYBOOK_ADDONS__,{addons:r,types:m,mockChannel:O}=__STORYBOOK_ADDONS__;var S=__STORYBOOK_THEMING__,{CacheProvider:f,ClassNames:g,Global:P,ThemeProvider:C,background:N,color:w,convert:x,create:s,createCache:y,createGlobal:R,createReset:k,css:v,darken:B,ensure:D,ignoreSsrWarning:G,isPropValid:K,jsx:Y,keyframes:A,lighten:E,styled:H,themes:I,typography:M,useTheme:W,withTheme:j}=__STORYBOOK_THEMING__;var a=s({base:"light",brandTitle:"WCPOS",brandUrl:"https://wcpos.com"});r.setConfig({isFullscreen:!1,showNav:!0,showPanel:!0,panelPosition:"bottom",sidebarAnimations:!0,enableShortcuts:!0,isToolshown:!0,theme:a,selectedPanel:void 0});})();
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
